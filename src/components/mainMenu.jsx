04e439a33a921c35b5e9099d524cf7647e91c4e1
Jacob Feng <peanutChowder@gmail.com>
2023-12-02 00:40:59 -0700

    Updated to work in react

--- src/components/mainMenu.jsx
+++ src/components/mainMenu.jsx
@@ -1,24 +1,35 @@
+import { useState } from 'react';
 import './mainMenu.scss'
 
 const MainMenu = () => {
+    const [menuOpen, setMenuOpen] = useState("open");
+
+    const handleOnMenuClick = () => {
+        if (menuOpen == "open") {
+            setMenuOpen("close");
+        } else {
+            setMenuOpen("open") 
+        }
+    }
+
     return (
-        <nav className="top-right open">
-        <a className="disc l1">
-            <div>Messages</div>
-        </a>
-        <a className="disc l2">
-            <div>Photos</div>
-        </a>
-        <a className="disc l3">
-            <div>Profile</div>
-        </a>
-        <a className="disc l4">
-            <div>Likes</div>
-        </a>
-        <a className="disc l5 toggle">
-            Menu
-        </a>
-</nav>
+        <nav className={`top-right ${menuOpen}`}>
+            <a className="disc l1">
+                <div>Messages</div>
+            </a>
+            <a className="disc l2">
+                <div>Photos</div>
+            </a>
+            <a className="disc l3">
+                <div>Profile</div>
+            </a>
+            <a className="disc l4">
+                <div>Likes</div>
+            </a>
+            <a className="disc l5 toggle" onClick={handleOnMenuClick}>
+                Menu
+            </a>
+        </nav>
     )
 }
 
