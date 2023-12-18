ec8f8fe6bf073e60dfecaf42693cf551541b875d
Jacob Feng <peanutChowder@gmail.com>
2023-12-18 01:41:08 -0700

    Added home button

--- src/pages/fun.jsx
+++ src/pages/fun.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 import CircleMenu from '../components/circleMenu';
+import HomeButton from '../components/homeButton';
 
 const Fun = ({ menuOpen, setMenuOpen }) => {
     return (
         <div>
+            <HomeButton/>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         </div>
     )
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 import CircleMenu from '../components/circleMenu';
+import HomeButton from '../components/homeButton';
 
 const Projects = ({ menuOpen, setMenuOpen }) => {
     return (
         <div>
+            <HomeButton/>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         </div>
     )
