5d733154ee9ebf5f3660d6dc508235df5f153419
Jacob Feng <peanutChowder@gmail.com>
2023-12-18 01:42:29 -0700

    Added button navigate to home functionality

--- src/components/homeButton.jsx
+++ src/components/homeButton.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
 import "./homeButton.css"
 
 const HomeButton = () => {
+    const navigate = useNavigate()
     return (
-        <div className="home-wrapper">
+        <div className="home-wrapper" onClick={() => navigate("/")}>
             <i className="gg-home-alt"></i>
         </div>
         
