29536bbe0d3407c816a07368a4c804df4d8bfd90
Jacob Feng <peanutChowder@gmail.com>
2023-12-18 00:31:33 -0700

    Refactor menu into component

--- src/pages/mainMenu.jsx
+++ src/pages/mainMenu.jsx
@@ -1,35 +1,15 @@
 import { useState } from 'react';
-import './mainMenu.scss'
+
+import CircleMenu from '../components/circleMenu';
+
 
 const MainMenu = () => {
     const [menuOpen, setMenuOpen] = useState("close");
 
-    const handleOnMenuClick = () => {
-        if (menuOpen == "open") {
-            setMenuOpen("close");
-        } else {
-            setMenuOpen("open") 
-        }
-    }
-
     return (
-        <nav className={`top-right ${menuOpen}`}>
-            <a className="disc l1">
-                <div>Resume</div>
-            </a>
-            <a className="disc l2">
-                <div>Projects</div>
-            </a>
-            <a className="disc l3">
-                <div>Sport</div>
-            </a>
-
-            <a 
-            className="disc l4 toggle" 
-            onClick={handleOnMenuClick}>
-                Menu
-            </a>
-        </nav>
+        <div>
+            <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        </div>
     )
 }
 
