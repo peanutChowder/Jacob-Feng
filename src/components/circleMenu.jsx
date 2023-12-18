6815620e5679939efa307178804ec59f217628c5
Jacob Feng <peanutChowder@gmail.com>
2023-12-18 00:28:51 -0700

    Refactor menu to separate components

---
+++ src/components/circleMenu.jsx
@@ -0,0 +1,40 @@
+import { propTypes } from 'prop-types';
+import './circleMenu.scss'
+
+const CircleMenu = ({ menuOpen, setMenuOpen }) => {
+
+    const handleOnMenuClick = () => {
+        if (menuOpen == "open") {
+            setMenuOpen("close");
+        } else {
+            setMenuOpen("open") 
+        }
+    }
+
+    return (
+        <nav className={`top-right ${menuOpen}`}>
+            <a className="disc l1">
+                <div>Resume</div>
+            </a>
+            <a className="disc l2">
+                <div>Projects</div>
+            </a>
+            <a className="disc l3">
+                <div>Sport</div>
+            </a>
+
+            <a 
+            className="disc l4 toggle" 
+            onClick={handleOnMenuClick}>
+                Menu
+            </a>
+        </nav>
+    )
+}
+
+CircleMenu.propTypes = {
+    menuOpen: propTypes.bool.isRequired,
+    setMenuOpen: propTypes.func.isRequired
+}
+
+export default CircleMenu
