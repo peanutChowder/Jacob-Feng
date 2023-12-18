992a6ef4fac9a617e2a29779f2651bb485bca2c1
Jacob Feng <peanutChowder@gmail.com>
2023-12-18 13:18:38 -0700

    Add header and flexbox display

--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import CircleMenu from '../components/circleMenu';
 import HomeButton from '../components/homeButton';
 
+import "./resume.css"
+
 const Resume = ({ menuOpen, setMenuOpen }) => {
     return (
         <div>
             <HomeButton/>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <div className='content'>
+                <h1>Professional Experience</h1>
+            </div>
         </div>
     )
 }
