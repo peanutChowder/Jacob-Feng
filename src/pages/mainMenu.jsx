75b3afe3676584ae368e1e775e670e7378954333
Jacob Feng <peanutChowder@gmail.com>
2023-12-20 19:01:31 -0700

    Added initial content format

--- src/pages/mainMenu.jsx
+++ src/pages/mainMenu.jsx
@@ -10,7 +10,19 @@ const MainMenu = ({ menuOpen, setMenuOpen }) => {
     return (
         <div>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-            <div id='oly-gif'></div>
+            <div id='mainpage-content'>
+                <div id='mainpage-oly-gif-container'>
+                    <div id='mainpage-oly-gif'></div>
+                    <em>The lift that brought home the podium.</em>
+                </div>
+                <div id='mainpage-content-text'>
+                    <h1>Jacob Feng</h1>
+                    <h2>Seeking Summer 2024 8-month internships</h2>
+                    <p>Software Engineering @ University of Alberta</p>
+                    <p>Olympic Weightlifter</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
