51608a00fd84ee93acb8b418dbabfc1a10ab95b4
Jacob Feng <peanutChowder@gmail.com>
2023-12-21 14:37:26 -0700

    Added scrolling display

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,11 +3,30 @@ import PropTypes from 'prop-types';
 import CircleMenu from '../components/circleMenu';
 import HomeButton from '../components/homeButton';
 
+import './projects.css'
+
 const Projects = ({ menuOpen, setMenuOpen }) => {
     return (
         <div>
             <HomeButton/>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <div id='projects-content'>
+            <section>
+                <h1>slide 1</h1>
+            </section>
+            <section>
+                <h1>slide 2</h1>
+            </section>
+            <section>
+                <h1>slide 3</h1>
+            </section>
+            <section>
+                <h1>slide 4</h1>
+            </section>
+            <section>
+                <h1>slide 5</h1>
+            </section>
+            </div>
         </div>
     )
 }
