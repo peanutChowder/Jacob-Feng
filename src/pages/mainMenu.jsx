06ab6772039f3ae972ba0c93aa63576bcc7f81c7
Jacob Feng <peanutChowder@gmail.com>
2023-12-20 19:12:30 -0700

    Added fade in animation

--- src/pages/mainMenu.jsx
+++ src/pages/mainMenu.jsx
@@ -11,15 +11,15 @@ const MainMenu = ({ menuOpen, setMenuOpen }) => {
         <div>
             <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
             <div id='mainpage-content'>
-                <div id='mainpage-oly-gif-container'>
+                <div id='mainpage-oly-gif-container'  className='fi-element fade-in'>
                     <div id='mainpage-oly-gif'></div>
                     <em>The lift that brought home the podium. Hover to watch.</em>
                 </div>
-                <div id='mainpage-content-text'>
+                <div id='mainpage-content-text' className='fi-element fade-in'>
                     <h1>Jacob Feng</h1>
-                    <h2>Seeking Summer 2024 8-month internships</h2>
-                    <p>Software Engineering @ University of Alberta</p>
-                    <p>Olympic Weightlifter</p>
+                    <h2 className='fi-element fade-in'>Seeking Summer 2024 8-month internships</h2>
+                    <p className='fi-element fade-in'>Software Engineering @ University of Alberta</p>
+                    <p className='fi-element fade-in'>Olympic Weightlifter</p>
                 </div>
             </div>
             
