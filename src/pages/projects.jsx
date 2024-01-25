6b3464e15bd0125bb881bb11530bb4d4deee135f
Jacob Feng <peanutchowder@gmail.com>
2024-01-25 14:49:22 -0700

    Implemented new projects component

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
 import CircleMenu from '../components/circleMenu';
 import HomeButton from '../components/homeButton';
 
 import './projects.css'
+
+import Project from '../components/project';
 
 import inventoryManagementApp from '../assets/inventoryTrackerDemo.gif'
 import kesslerFuzzy from '../assets/kesslerFuzzyDemo.gif'
@@ -15,6 +17,42 @@ import coolGpt from '../assets/coolGptDemo.gif'
 
 const Projects = ({ menuOpen, setMenuOpen }) => {
     window.scrollTo(0, 0);
+
+    const projectList = [
+        {
+            id: 'itac-demo',
+            name: 'Inventory Management App',
+            gif: inventoryManagementApp,
+            url: 'https://github.com/CMPUT301F23T29/agile-beast',
+            techStack: 'Tech stack: Android Studio + Java, Firebase.',
+            description: 'Built in an Agile team of 6. Inventory tracking made easy with features such as quick barcode scanning and item lookup on Amazon/Google.'
+        },
+        {
+            id: 'kf-demo',
+            name: 'Kessler Fuzzy System',
+            gif: kesslerFuzzy,
+            url: 'https://github.com/peanutChowder/XFC-fuzzy-agent',
+            techStack: 'Tech stack: Python, Scikit-Fuzzy, EasyGA',
+            description: 'Optimized with genetic algorithms. Uses fuzzy inference to maximize asteroid destruction, minimize collisions.'
+        },
+        {
+            id: 'cj-demo',
+            name: 'Concurrent command line',
+            gif: concurrentJob,
+            url: 'https://github.com/peanutChowder/Shell-Simulator',
+            techStack: 'Tech stack: C++/C',
+            description: 'Tackles the challenges of concurrent programming without hiccups. Concurrent job management is the foundation that all modern software is built on.'
+        },
+        {
+            id: 'cg-demo',
+            name: 'GPT Summarizer',
+            gif: coolGpt,
+            url: 'https://github.com/GPTGDSC/cool-gpt-app',
+            techStack: 'Tech stack: Vite.js + React, Flask',
+            description: 'Harnesses the power of ChatGPT to summarize PDFs and raw text.'
+        },
+    ]
+
     useEffect(() => {
         const { innerWidth: width, } = window
         if (width <= 830) {
@@ -23,77 +61,30 @@ const Projects = ({ menuOpen, setMenuOpen }) => {
     }, [setMenuOpen])
 
     useEffect(() => {
-        document.addEventListener('scroll', () => {console.log(document.documentElement.scrollTop)});
+        document.addEventListener('scroll', () => { console.log(document.documentElement.scrollTop) });
     })
     return (
         <div>
-            <HomeButton/>
-            <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <HomeButton />
+            <CircleMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             <div id='projects-content'>
-            <section>
-                <div className='projects-content-container fi-element fade-in'>
-                    <div>
-                        <img id='itac-demo' className='demo' src={inventoryManagementApp}/>
+                {projectList.map((project) => {
+                    return <Project
+                        key={project.id}
+                        id={project.id}
+                        name={project.name}
+                        gif={project.gif}
+                        url={project.url}
+                        techStack={project.techStack}
+                        description={project.description}
+                    />
+                })}
+                <section>
+                    <div className='projects-content-container'>
+                        <h1>More work on <a href="https://github.com/peanutChowder">GitHub</a></h1>
                     </div>
-                    <div className='projects-content-text'>
-                        <h1>Inventory Management App</h1>
-                        <h2>(<a href='https://github.com/CMPUT301F23T29/agile-beast'>Github</a>) Tech stack: Android Studio + Java, Firebase.</h2>
-                        <p>Built in an Agile team of 6. Inventory tracking made easy with features such as quick barcode scanning and item lookup on Amazon/Google.</p>
-                    </div>
-                    <div id='project-scroll-hint'>
-                        <p>Scroll Down</p>
-                        <FontAwesomeIcon icon={faArrowAltCircleDown}/>
-                    </div>
-                    
-                </div>
-            </section>
-            <section>
-                <div className='projects-content-container fi-element fade-in'>
-                    <div>
-                        <img id='kf-demo' className='demo' src={kesslerFuzzy}/>
-                    </div>
-                    <div className='projects-content-text'>
-                        <h1>Kessler Fuzzy System</h1>
-                        <h2>(<a href='https://github.com/peanutChowder/XFC-fuzzy-agent'>Github</a>) Tech stack: Python, Scikit-Fuzzy, EasyGA</h2>
-                        <p>Optimized with genetic algorithms. Uses fuzzy inference to maximize asteroid destruction, minimize collisions.</p>
-                    </div>
-                    <div id='project-scroll-hint'>
-                        <p>Scroll Down</p>
-                        <FontAwesomeIcon icon={faArrowAltCircleDown}/>
-                    </div>
-                    
-                </div>
-            </section>
-            <section>
-                <div className='projects-content-container'>
-                    <div>
-                        <img id='cj-demo' className='demo' src={concurrentJob}/>
-                    </div>      
-                    <div className='projects-content-text'>
-                        <h1>Concurrent command line</h1>
-                        <h2>(<a href='https://github.com/peanutChowder/Shell-Simulator'>Github</a>) Tech stack: C++/C</h2>
-                        <p>Tackles the challenges of concurrent programming without hiccups. Concurrent job management is the foundation that all modern software is built on.</p>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div className='projects-content-container'>
-                    <div>
-                        <img id='cg-demo' className='demo' src={coolGpt}/>
-                    </div>
-                    <div className='projects-content-text'>
-                        <h1>GPT Summarizer</h1>
-                        <h2>(<a href='https://github.com/GPTGDSC/cool-gpt-app'>Github</a>) Tech stack: Vite.js + React, Flask</h2>
-                        <p>Harnesses the power of ChatGPT to summarize PDFs and raw text.</p>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div className='projects-content-container'>
-                    <h1>More work on <a href="https://github.com/peanutChowder">GitHub</a></h1>
-                </div>
-                
-            </section>
+
+                </section>
             </div>
         </div>
     )
