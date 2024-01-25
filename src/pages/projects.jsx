a6c04404dd8391e153f51ac25e2bcfd05ff1cebf
Jacob Feng <peanutchowder@gmail.com>
2024-01-25 15:01:58 -0700

    Separated projects list to asset file

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,49 +7,11 @@ import HomeButton from '../components/homeButton';
 import './projects.css'
 
 import Project from '../components/project';
+import projectList from '../assets/projectsList';
 
-import inventoryManagementApp from '../assets/inventoryTrackerDemo.gif'
-import kesslerFuzzy from '../assets/kesslerFuzzyDemo.gif'
-import concurrentJob from '../assets/concurrentJobManagerDemo.gif'
-import coolGpt from '../assets/coolGptDemo.gif'
 
 const Projects = ({ menuOpen, setMenuOpen }) => {
     window.scrollTo(0, 0);
-
-    const projectList = [
-        {
-            id: 'itac-demo',
-            name: 'Inventory Management App',
-            gif: inventoryManagementApp,
-            url: 'https://github.com/CMPUT301F23T29/agile-beast',
-            techStack: 'Tech stack: Android Studio + Java, Firebase.',
-            description: 'Built in an Agile team of 6. Inventory tracking made easy with features such as quick barcode scanning and item lookup on Amazon/Google.'
-        },
-        {
-            id: 'kf-demo',
-            name: 'Kessler Fuzzy System',
-            gif: kesslerFuzzy,
-            url: 'https://github.com/peanutChowder/XFC-fuzzy-agent',
-            techStack: 'Tech stack: Python, Scikit-Fuzzy, EasyGA',
-            description: 'Optimized with genetic algorithms. Uses fuzzy inference to maximize asteroid destruction, minimize collisions.'
-        },
-        {
-            id: 'cj-demo',
-            name: 'Concurrent command line',
-            gif: concurrentJob,
-            url: 'https://github.com/peanutChowder/Shell-Simulator',
-            techStack: 'Tech stack: C++/C',
-            description: 'Tackles the challenges of concurrent programming without hiccups. Concurrent job management is the foundation that all modern software is built on.'
-        },
-        {
-            id: 'cg-demo',
-            name: 'GPT Summarizer',
-            gif: coolGpt,
-            url: 'https://github.com/GPTGDSC/cool-gpt-app',
-            techStack: 'Tech stack: Vite.js + React, Flask',
-            description: 'Harnesses the power of ChatGPT to summarize PDFs and raw text.'
-        },
-    ]
 
     useEffect(() => {
         const { innerWidth: width, } = window
