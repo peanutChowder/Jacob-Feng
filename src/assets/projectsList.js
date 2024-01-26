bee357c26d50a03b233693290711e193da27d54b
Jacob Feng <peanutchowder@gmail.com>
2024-01-26 10:15:50 -0700

    Update project text

--- src/assets/projectsList.js
+++ src/assets/projectsList.js
@@ -10,7 +10,7 @@ const projectList = [
         gif: inventoryManagementApp,
         url: 'https://github.com/CMPUT301F23T29/agile-beast',
         techStack: 'Tech stack: Android Studio + Java, Firebase.',
-        description: 'Built in an Agile team of 6. Inventory tracking made easy with features such as quick barcode scanning and item lookup on Amazon/Google.',
+        description: 'Inventory tracking made easy with features such as quick barcode scanning and item lookup on Amazon/Google.',
         shortName: 'Inventory App'
     },
     {
@@ -19,7 +19,7 @@ const projectList = [
         gif: kesslerFuzzy,
         url: 'https://github.com/peanutChowder/XFC-fuzzy-agent',
         techStack: 'Tech stack: Python, Scikit-Fuzzy, EasyGA',
-        description: 'Optimized with genetic algorithms. Uses fuzzy inference to maximize asteroid destruction, minimize collisions.',
+        description: 'Optimized fitness using a genetic algorithm. A fuzzy system to maximize asteroid destruction and minimize collisions.',
         shortName: 'Genetic bot'
     },
     {
@@ -38,7 +38,7 @@ const projectList = [
         url: 'https://github.com/GPTGDSC/cool-gpt-app',
         techStack: 'Tech stack: Vite.js + React, Flask',
         description: 'Harnesses the power of ChatGPT to summarize PDFs and raw text.',
-        shortName: 'GPT Summarizer'
+        shortName: 'AI Summary'
     },
 ]
 
