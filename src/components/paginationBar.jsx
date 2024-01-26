acbe198763ba1ac0fa672666c4e86240973e8851
Jacob Feng <peanutchowder@gmail.com>
2024-01-25 23:07:24 -0700

    Removed unused symbols

--- src/components/paginationBar.jsx
+++ src/components/paginationBar.jsx
@@ -48,12 +48,6 @@ const PaginationBar = () => {
                     More
                 </div>
             </div>
-
-            <svg className="hide">
-                <symbol id="left" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></symbol>
-                <symbol id="right" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></symbol>
-            </svg>
-
         </>
     )
 }
