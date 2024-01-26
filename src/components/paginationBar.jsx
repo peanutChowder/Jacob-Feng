d99a4638bb9c26f9c214146e2296a327816e80e5
Jacob Feng <peanutchowder@gmail.com>
2024-01-26 10:41:45 -0700

    Added dynamic getter for pagination button complete width

--- src/components/paginationBar.jsx
+++ src/components/paginationBar.jsx
@@ -1,27 +1,35 @@
 import './paginationBar.scss'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 import projectList from '../assets/projectsList'
 
 const PaginationBar = () => {
+    const marginAndPadding = 13
+    const paginationButton = useRef(null)
     const [currPage, setCurrPage] = useState(Math.floor((document.documentElement.scrollTop + 20) / window.innerHeight) + 1)
-    const paginationWidth = 163
     const [translateX, setTranslateX] = useState(0)
-
+    const [paginationButtonWidth, setPaginationButtonWidth] = useState(0)
+    
     useEffect(() => {
         const handleScroll = () => {
             const page = Math.floor((document.documentElement.scrollTop + 20) / window.innerHeight) + 1
             
             if (page !== currPage) {
                 setCurrPage(page) 
-                setTranslateX(-paginationWidth * (page - 1))
-            }          
+                setTranslateX(-paginationButtonWidth * (page - 1))
+            }        
         }
         document.addEventListener('scroll', handleScroll)
 
         return () => {document.removeEventListener('scroll', handleScroll)}
     })
+
+    useEffect(() => {
+        if (paginationButton) {
+            setPaginationButtonWidth(paginationButton.current.offsetWidth + marginAndPadding)
+        }
+    }, [])
 
     const handlePaginationClick = (pageIndex) => {        
         window.scrollTo({
@@ -33,11 +41,12 @@ const PaginationBar = () => {
     return (
         <>
             <div className='hidden-pagination'
-                style={{width: `${paginationWidth * 5}px`}}>
+                style={{width: `${paginationButtonWidth * 5}px`}}>
                 <div className="pagination:container">
                     {projectList.map((project, pageIndex) => {
                         return (
                             <div 
+                                ref={paginationButton}
                                 key={pageIndex}
                                 className={`pagination:number ${(pageIndex + 1) === currPage ? 'pagination:active' : ''}`}
                                 onClick={() => {handlePaginationClick(pageIndex)}}
