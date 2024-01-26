717297dbf35ed08e4b4e4fdd1b2f49eb39356649
Jacob Feng <peanutchowder@gmail.com>
2024-01-26 11:01:45 -0700

    Added dynamic adjusting of number of pagination buttons shown

--- src/components/paginationBar.jsx
+++ src/components/paginationBar.jsx
@@ -7,16 +7,17 @@ import projectList from '../assets/projectsList'
 const PaginationBar = () => {
     const marginAndPadding = 13
     const paginationButton = useRef(null)
-    const [currPage, setCurrPage] = useState(Math.floor((document.documentElement.scrollTop + 20) / window.innerHeight) + 1)
+    const [currPageNum, setcurrPageNum] = useState(Math.floor((document.documentElement.scrollTop + 20) / window.innerHeight) + 1)
     const [translateX, setTranslateX] = useState(0)
     const [paginationButtonWidth, setPaginationButtonWidth] = useState(0)
+    const [numPaginationButtonsShown, setNumPaginationButtonsShown] = useState(3)
     
     useEffect(() => {
         const handleScroll = () => {
             const page = Math.floor((document.documentElement.scrollTop + 20) / window.innerHeight) + 1
             
-            if (page !== currPage) {
-                setCurrPage(page) 
+            if (page !== currPageNum) {
+                setcurrPageNum(page) 
                 setTranslateX(-paginationButtonWidth * (page - 1))
             }        
         }
@@ -28,8 +29,19 @@ const PaginationBar = () => {
     useEffect(() => {
         if (paginationButton) {
             setPaginationButtonWidth(paginationButton.current.offsetWidth + marginAndPadding)
+
+            const numButtonsFit = Math.floor(window.innerWidth / paginationButtonWidth)
+
+            if (numButtonsFit >= 5) {
+                setNumPaginationButtonsShown(5)
+            } else if (numButtonsFit <= 3) {
+                setNumPaginationButtonsShown(3)
+            } else if (numButtonsFit % 2 == 0) {
+                setPaginationButtonWidth(numButtonsFit - 1)
+            }
+            
         }
-    }, [])
+    }, [paginationButton, paginationButtonWidth])
 
     const handlePaginationClick = (pageIndex) => {        
         window.scrollTo({
@@ -41,14 +53,14 @@ const PaginationBar = () => {
     return (
         <>
             <div className='hidden-pagination'
-                style={{width: `${paginationButtonWidth * 5}px`}}>
+                style={{width: `${paginationButtonWidth * numPaginationButtonsShown}px`}}>
                 <div className="pagination:container">
                     {projectList.map((project, pageIndex) => {
                         return (
                             <div 
                                 ref={paginationButton}
                                 key={pageIndex}
-                                className={`pagination:number ${(pageIndex + 1) === currPage ? 'pagination:active' : ''}`}
+                                className={`pagination:number ${(pageIndex + 1) === currPageNum ? 'pagination:active' : ''}`}
                                 onClick={() => {handlePaginationClick(pageIndex)}}
                                 style={{transform: `translate(${translateX}px)`}}>
                             {project.shortName}
@@ -56,7 +68,7 @@ const PaginationBar = () => {
                         )
                     })}
                     <div
-                        className={`pagination:number ${currPage === 5 ? 'pagination:active' : ''}`}
+                        className={`pagination:number ${currPageNum === 5 ? 'pagination:active' : ''}`}
                         onClick={() => {handlePaginationClick(5)}}
                         style={{transform: `translate(${translateX}px)`}}
                     >
